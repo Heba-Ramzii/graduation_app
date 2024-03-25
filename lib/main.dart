56abc6677718fdddf1dc9f42ce46ec30a958f0aa
yahya <20912020101108@fci.zu.edu.eg>
2024-03-25 20:17:41 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+//yahia
 void main() {
   runApp(const MyApp());
 }
@@ -13,12 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       //home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-
-
