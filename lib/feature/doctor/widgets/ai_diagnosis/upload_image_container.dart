3d66d341bb3e1a02bed7abfee4f1c5ed982d414b
Heba-Ramzii <hebaramzii33@gmail.com>
2024-05-02 00:08:24 +0300

    ai**

--- lib/feature/doctor/widgets/ai_diagnosis/upload_image_container.dart
+++ lib/feature/doctor/widgets/ai_diagnosis/upload_image_container.dart
@@ -3,6 +3,7 @@ import 'package:graduation_app/core/core_widgets/custom_material_button.dart';
 import 'package:graduation_app/core/theme_manager/colors_manager.dart';
 import 'package:graduation_app/core/theme_manager/style_manager.dart';
 import 'package:graduation_app/generated/assets.dart';
+import 'package:mobkit_dashed_border/mobkit_dashed_border.dart';
 
 class UploadImageContainer extends StatelessWidget {
   const UploadImageContainer({super.key});
@@ -10,24 +11,28 @@ class UploadImageContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: const ShapeDecoration(
-        shape: RoundedRectangleBorder(
-        side: BorderSide(
-        width: 2,
-        strokeAlign: BorderSide.strokeAlignCenter,
-        color: ColorsManager.primaryBorder,
-    ),
-    ),
-    ),
+       padding: EdgeInsets.all(32),
+      decoration: const BoxDecoration(
+          border: DashedBorder.fromBorderSide(
+              dashLength: 15,
+              side: BorderSide(
+                  color: ColorsManager.primaryBorder,
+                  width: 2)
+           ),
+          borderRadius: BorderRadius.all(Radius.circular(2))
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset(
-              Assets.imagesUploadCloud,
-            height: 100,
-            width: 100,
+          Padding(
+            padding: const EdgeInsets.only(top: 4,bottom: 15),
+            child: Image.asset(
+                Assets.imagesUploadCloud,
+              height: 100,
+              width: 100,
+            ),
           ),
-          Text(
+           Text(
               "Drag or drop file here",
             style: StyleManager.buttonTextStyle16.copyWith(
               color: ColorsManager.primaryLight,
