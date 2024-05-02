25ef223015857bdc766df187835a17c400577988
yahya <20912020101108@fci.zu.edu.eg>
2024-05-02 23:24:24 +0300

    Update dependencies in pubspec.yaml

---
+++ lib/core/core_widgets/custom_icon_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../theme_manager/colors_manager.dart';
+
+
+class CustomIconButton extends StatelessWidget {
+  const CustomIconButton({super.key,
+    this.iconColor=ColorsManager.primary,
+    this.iconSize = 24,
+    required this.onPressed,
+    required this.icon
+});
+  final String icon;
+  final Color iconColor;
+final double iconSize ;
+final Function() onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return  InkWell(
+      onTap: onPressed,
+      child: SvgPicture.asset(
+        icon,
+         height: iconSize,
+        width: iconSize,
+        color: iconColor,
+      ),
+    );
+  }
+}
---
+++ lib/core/theme_manager/style_manager.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'colors_manager.dart';
+
+abstract class StyleManager {
+  // title
+  static const textStyle32 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w900,
+  );
+
+   static const buttonTextStyle16 = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      color: ColorsManager.white
+
+
+  );
+
+
+  static const mainTextStyle15 = TextStyle(
+    fontSize: 15,
+  );
+
+  static const warringTextStyle15 = TextStyle(
+    fontSize: 15,
+    color: ColorsManager.red,
+  );
+
+
+  static const textStyle14= TextStyle(
+      fontSize: 14,
+   );
+
+  static const textStyle14mid = TextStyle(
+      fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+
+  //label
+  static const textStyle12 = TextStyle(
+      fontSize: 12,
+      color: ColorsManager.primary
+  );
+
+}
