64cf43b1fbd85074c6a3a954fb405ecb35dfe9d7
yahya <20912020101108@fci.zu.edu.eg>
2024-05-02 23:55:57 +0300

    chore: Update dependencies and add new color constants to ColorsManager

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:graduation_app/core/theme_manager/colors_manager.dart';
 import 'package:graduation_app/layout/patient_home_screen.dart';
 
 void main() {
@@ -12,13 +13,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Graduation App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        iconTheme: const IconThemeData(
+          color: ColorsManager.primary,
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: ColorsManager.primaryMaterialColor,
       ),
+      debugShowCheckedModeBanner: false,
       home: const PatientHome(),
-      //test
     );
   }
 }
