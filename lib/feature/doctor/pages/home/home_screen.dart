2cc6388f9a10a917462f76e124c6b70397e3ea0e
Heba-Ramzii <hebaramzii33@gmail.com>
2024-05-04 01:21:49 +0300

    ai_upload**

--- lib/feature/doctor/pages/home/home_screen.dart
+++ lib/feature/doctor/pages/home/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:graduation_app/feature/doctor/widgets/home/ai_features_section.dart';
 import 'package:graduation_app/feature/doctor/widgets/home/appointment_section/appointment_section.dart';
 import 'package:graduation_app/feature/doctor/widgets/home/user_info.dart';
+
+import '../../../../core/theme_manager/colors_manager.dart';
 
 
 class HomeDoctorScreen extends StatelessWidget {
@@ -9,8 +12,8 @@ class HomeDoctorScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
+    return Scaffold(
+      body: const Column(
         children: [
           UserInfoSection(),
           Padding(
@@ -25,6 +28,43 @@ class HomeDoctorScreen extends StatelessWidget {
           ),
          ],
       ),
+      bottomNavigationBar: NavigationBar(
+        height: 70,
+        indicatorShape: const CircleBorder(
+          eccentricity: 0.0,
+        ),
+        backgroundColor: ColorsManager.primaryLight2,
+        selectedIndex: 0,
+        //indicatorColor: ColorsManager.white,
+        onDestinationSelected: (index) {},
+        destinations: [
+          NavigationDestination(
+              icon: SvgPicture.asset(
+                'assets/icons/home.svg',
+                color: ColorsManager.primary,
+              ),
+              label: ''),
+          NavigationDestination(
+              icon: SvgPicture.asset(
+                'assets/icons/chat-alt.svg',
+                color: ColorsManager.primary,
+              ),
+              label: ''),
+          NavigationDestination(
+              icon: SvgPicture.asset(
+                'assets/icons/calendar.svg',
+                color: ColorsManager.primary,
+              ),
+              label: ''),
+          NavigationDestination(
+              icon: SvgPicture.asset(
+                'assets/icons/dots-horizontal.svg',
+                color: ColorsManager.primary,
+              ),
+              label: ''),
+        ],
+      ),
+
     );
   }
 }
