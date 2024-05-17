b99fd6457783ca83ef1a4b4f9c0106f583e94921
Heba-Ramzii <hebaramzii33@gmail.com>
2024-05-17 19:09:08 +0300

    heba+schedule

--- lib/feature/patient/layout/schedule_screen.dart
+++ lib/feature/patient/layout/schedule_screen.dart
@@ -1,14 +1,17 @@
 import 'package:date_picker_plus/date_picker_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:graduation_app/core/core_widgets/custom_app_bar.dart';
 import 'package:graduation_app/core/core_widgets/custom_material_button.dart';
 import 'package:graduation_app/core/core_widgets/custom_text_form_field.dart';
+import 'package:graduation_app/core/core_widgets/profile_image.dart';
 import 'package:graduation_app/core/theme_manager/colors_manager.dart';
 import 'package:graduation_app/core/theme_manager/style_manager.dart';
 import 'package:graduation_app/feature/patient/layout/patient_home_screen.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 
 import '../widget/clinic_card.dart';
+import 'continue_schedule.dart';
 
 class SchedualScreen extends StatefulWidget {
   const SchedualScreen({
@@ -32,11 +35,7 @@ class _SchedualScreenState extends State<SchedualScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.asset(
-              'assets/images/profile.png',
-              height: 200,
-              width: double.infinity,
-            ),
+            const ProfileImage(height: 200, width: double.infinity),
              Padding(
                padding: const EdgeInsets.only(top: 16.0,bottom: 6),
                child: Text(
@@ -66,102 +65,25 @@ class _SchedualScreenState extends State<SchedualScreen> {
               ),
             ),
             Container(
-              color: ColorsManager.white,
+              margin:const EdgeInsets.symmetric(vertical: 20.0) ,
+              decoration: StyleManager.containerDecoration,
               child: DatePicker(
-                minDate: DateTime(2021, 1, 1),
-                maxDate: DateTime(2023, 12, 31),
+                minDate: DateTime(2024, 1, 1),
+                maxDate: DateTime(2050, 12, 31),
                 onDateSelected: (value) {
                   // Handle selected date
                 },
               ),
             ),
-            const SizedBox(height: 50),
-            const SizedBox(height: 10),
-            const Divider(
-              color: ColorsManager.blue,
-              thickness: 2,
-            ),
-            const SizedBox(height: 5),
-            const Text(
-              "Patient Information",
-              style: TextStyle(
-                color: ColorsManager.black,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            ToggleSwitch(
-              minWidth: MediaQuery.of(context).size.width,
-              initialLabelIndex: 0,
-              totalSwitches: 2,
-              labels: const ['Your Self', 'Another Person'],
-              onToggle: (index) {
-                print('switched to: $index');
-              },
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              "Full Name",
-              style: TextStyle(
-                color: ColorsManager.primary,
-                fontSize: 16,
-              ),
-            ),
-            CustomTextFormField(
-              controller: controller1,
-              type: TextInputType.name,
-            ),
-             Text(
-              "Age",
-              style: StyleManager.buttonTextStyle16.copyWith(
-                  color: ColorsManager.primary
-              ),
-            ),
-            CustomTextFormField(
-              controller: controller1,
-              type: TextInputType.phone,
-            ),
-            ToggleSwitch(
-              minWidth: MediaQuery.of(context).size.width,
-              initialLabelIndex: 0,
-              totalSwitches: 2,
-              labels: const ['Male', 'Female'],
-              onToggle: (index) {
-                print('switched to: $index');
-              },
-            ),
-             Padding(
-               padding: const EdgeInsets.symmetric(vertical: 12.0),
-               child: Text(
-                "Describe Your Case",
-                style: StyleManager.buttonTextStyle16.copyWith(
-                    color: ColorsManager.font
-                ),
-                           ),
-             ),
-            TextFormField(
-              maxLines: 5,
-              decoration: InputDecoration(
-                hintText: 'Write your case here',
-                hintStyle: const TextStyle(
-                  color: ColorsManager.blue,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
             SizedBox(
               width: MediaQuery.of(context).size.width * 0.9,
               child: CustomMaterialButton(
-                text: "Submit",
+                text: "Continue",
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (BuildContext context) {
-                      return const PatientHome();
+                      return const ContinueScheduleScreen();
                     }),
                   );
                 },
