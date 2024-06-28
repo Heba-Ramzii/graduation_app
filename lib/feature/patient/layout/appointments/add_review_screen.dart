ee9db87e2d1ba89fa9dfbda25d46eb80bccc50c6
Heba-Ramzii <hebaramzii33@gmail.com>
2024-06-29 01:43:10 +0300

    appointment

--- lib/feature/patient/layout/appointments/add_review_screen.dart
+++ lib/feature/patient/layout/appointments/add_review_screen.dart
@@ -1,8 +1,10 @@
+import 'package:custom_radio_grouped_button/custom_radio_grouped_button.dart';
 import 'package:flutter/material.dart';
 import 'package:graduation_app/core/core_widgets/custom_app_bar.dart';
 import 'package:graduation_app/core/core_widgets/profile_image.dart';
 import 'package:graduation_app/feature/patient/widget/appointments/reason_section.dart';
 
+import '../../../../core/theme_manager/colors_manager.dart';
 import '../../../../core/theme_manager/style_manager.dart';
 
 class AddReviewScreen extends StatelessWidget {
@@ -26,10 +28,41 @@ class AddReviewScreen extends StatelessWidget {
                     fontWeight: FontWeight.w500
                 ),
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 24.0),
+             Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24.0),
               child: Column(
-                children: [],
+                children: [
+                  CustomRadioButton(
+                    horizontal:true,
+                    height: 45,
+                    elevation: 0,
+                    unSelectedColor: Theme.of(context).canvasColor,
+                    buttonLables: const [
+                      '5 Stars',
+                      '4 Stars',
+                      '3 Stars',
+                      '2 Stars',
+                      '1 Stars',
+                    ],
+                    buttonValues: const [
+                      '5 Stars',
+                      '4 Stars',
+                      '3 Stars',
+                      '2 Stars',
+                      '1 Stars',
+                    ],
+                    shapeRadius: 10,
+                    padding: 1,
+                    enableShape: true,
+                    buttonTextStyle: const ButtonTextStyle(
+                        selectedColor: Colors.white,
+                        unSelectedColor: ColorsManager.font,
+                        textStyle: StyleManager.textStyle18),
+                    radioButtonValue: (value) {
+                    },
+                    selectedColor: ColorsManager.primary,
+                  ),
+                ],
               ),
             ),
             const ReasonSection(title: "You can write a comment",),
--- lib/feature/patient/layout/appointments/cancel_screen.dart
+++ lib/feature/patient/layout/appointments/cancel_screen.dart
@@ -1,6 +1,8 @@
+import 'package:custom_radio_grouped_button/custom_radio_grouped_button.dart';
 import 'package:flutter/material.dart';
 import 'package:graduation_app/core/core_widgets/custom_app_bar.dart';
 
+import '../../../../core/theme_manager/colors_manager.dart';
 import '../../../../core/theme_manager/style_manager.dart';
 import '../../widget/appointments/reason_section.dart';
 
@@ -22,10 +24,39 @@ class CancelAppointmentScreen extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 24.0),
-              child: Column(
-                children: [],
+             Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24.0),
+              child:Column(
+                children: [
+                  CustomRadioButton(
+                    horizontal:true,
+                    height: 45,
+                    elevation: 0,
+                    unSelectedColor: Theme.of(context).canvasColor,
+                    buttonLables: const [
+                      'weather condition',
+                      'Rescheduling',
+                      'Unexpected Work',
+                      'Other',
+                    ],
+                    buttonValues: const [
+                      "weather",
+                      "Rescheduling",
+                      "Unexpected",
+                      "Other",
+                    ],
+                    shapeRadius: 10,
+                    padding: 1,
+                    enableShape: true,
+                    buttonTextStyle: const ButtonTextStyle(
+                        selectedColor: Colors.white,
+                        unSelectedColor: ColorsManager.font,
+                        textStyle: StyleManager.textStyle18),
+                    radioButtonValue: (value) {
+                    },
+                    selectedColor: ColorsManager.primary,
+                  ),
+                ],
               ),
             ),
             const ReasonSection(title: "Tell Us The reason",),
